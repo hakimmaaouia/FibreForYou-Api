67493384a61d58bf1f3739220fea80f6270851f8
hakim maaouia <hakimmaaouia@gmail.com>
2021-12-12 00:27:23 +0100

    add pre-commit :green_heart:

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import console from 'console';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('bootstrap');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
