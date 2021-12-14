b5badad3a5fd3a47727732b82587dc58b9af71e5
hakim maaouia <hakimmaaouia@gmail.com>
2021-12-14 22:43:11 +0100

    testing husky

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
-  console.log('Starting server...');
+ 
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
