dcf71b50e043b398fcbf2a149cfe1affc3f2a0fa
hakim maaouia <hakimmaaouia@gmail.com>
2021-12-14 22:35:07 +0100

    precommit testnig

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import console from 'console';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
-  console.log('bootstrap');
-  var test = 'test';
-  var test2 = 'test';
-
-  
+  console.log('Starting server...');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
